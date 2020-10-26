11e2b10a2611112c85eb6baa7336f917055bc5c1
lucasslemos <lucas.lemos.dev@gmail.com>
2020-10-26 12:22:32 -0400

    Criando a task dev:setup para configuração do anbiente com os logins padrão.

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
+  devise_for :users
   get 'a/index'
   get 'inicio', to: 'site/welcome#index'
 

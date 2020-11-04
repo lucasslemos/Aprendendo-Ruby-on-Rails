b6965939f479dd64115290efc8bbea653e1facc0
lucasslemos <lucas.lemos.dev@gmail.com>
2020-11-04 11:38:45 -0400

    Configurando SB ADM2

---
+++ time_to_answer6/app/javascript/packs/admins_backoffice.js
@@ -0,0 +1,23 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+//= require admins_backoffice/vendor/jquery/jquery.min
+//= require admins_backoffice/vendor/jquery-easing/jquery.easing.min
+//= require admins_backoffice/js/sb-admin-2.min
+//= require admins_backoffice/vendor/bootstrap/js/bootstrap.bundle.min.js
+
+
+require('./admins_backoffice/vendor/jquery/jquery.min')
+require('./admins_backoffice/vendor/jquery-easing/jquery.easing.min')
+require('./admins_backoffice/js/sb-admin-2.min')
+require('./admins_backoffice/vendor/bootstrap/js/bootstrap.bundle.min')
+

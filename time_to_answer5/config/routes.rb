ba46d3a470dd746be9211ef89167582f6da0d80f
lucasslemos <lucas.lemos.dev@gmail.com>
2020-11-12 13:16:56 -0400

    Modelando o cadastro de questoes e configrando as views de Assuntos_Areas

--- time_to_answer5/config/routes.rb
+++ time_to_answer5/config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'admins/index'
+    get 'welcome/index'
     resources :admins
+    resources :subjects
   end
+  
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  devise_for :users
+ 
   namespace :site do
     get 'welcome/index'
   end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
-  namespace :admins_backoffice do
-    get 'welcome/index'
-  end
+
   devise_for :admins
+  devise_for :users
 
   get 'inicio', to: 'site/welcome#index'
 

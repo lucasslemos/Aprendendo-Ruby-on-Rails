b079c4b6814422a6165f258065e62f5856b7dbe9
lucasslemos <lucas.lemos.dev@gmail.com>
2020-11-18 12:12:40 -0400

    Ajustando o layout do admim/davice e corrigindo erro de excluir de questions e subjects

--- time_to_answer5/app/controllers/admins_backoffice/questions_controller.rb
+++ time_to_answer5/app/controllers/admins_backoffice/questions_controller.rb
@@ -4,11 +4,8 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def index
     @questions = Question.includes(:subject)
-                        .order(:description)
-                        .page(params[:page])
-  end
-
-  def edit
+                         .order(:id)
+                         .page(params[:page])
   end
 
   def new
@@ -24,26 +21,30 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
     end
   end
 
-  def update
-    if @question.update(params_question)
-      redirect_to admins_backoffice_questions_path, notice: "Questão autalizada com sucesso!"
+  def edit
+  end
+
+  def update    
+    if  @question.update(params_question)
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizado com sucesso!"
     else
       render :edit
     end
   end
 
   def destroy
-    if @question.destroy
-      redirect_to admins_backoffice_questions_path, notice: "Apagando Questão!"
+    if  @question.destroy
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
     else
       render :index
     end
   end
 
   private
-  def params_question 
-    params.require(:question).permit(:description, :subject_id, 
-        answers_attributes: [:id, :description, :correct, :_destroy])
+  
+  def params_question
+    params.require(:question).permit(:description, :subject_id,
+       answers_attributes: [:id, :description, :correct, :_destroy])
   end
 
   def set_question
@@ -52,6 +53,5 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def get_subjects
     @subjects = Subject.all
-  end 
-
+  end
 end
